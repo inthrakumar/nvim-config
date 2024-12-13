a6c5d8bdb5be85b625a6c9fdbc08ff105848035e
inthrakumar <inthrak04@gmail.com>
2024-12-13 14:46:10 +0530

    bug fix in telescope and null_ls config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,19 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+        require("neo-tree").setup({filesystem = {
+          follow_current_file = true,
+          hijack_netrw_behavior = "open_default",
+        }})
+            vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("neo-tree.command").execute({ dir = vim.loop.cwd() })
+    end
+  end,
+})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -1,3 +1,15 @@
 vim.keymap.set("n","<C-a>",":Alpha <CR>")
 vim.keymap.set("n","<C-e>",vim.cmd.Ex)
 vim.keymap.set("n","<C-l>",":Lazy <CR>")
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("neo-tree.command").execute({ toggle = true })
+    end
+  end,
+})

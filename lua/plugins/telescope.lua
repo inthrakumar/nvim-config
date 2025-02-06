1da53b145f28fb67c8121cb260523da0c245746c
inthrakumar <inthrak04@gmail.com>
2025-02-06 23:07:44 +0530

    changes in keymaps for telescope nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,37 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", function()
-				builtin.find_files({ hidden = true })
-			end, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+                },
+                pickers = {
+                    find_files = {
+                        theme = "ivy",
+                    },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>p", function()
+                builtin.find_files({ hidden = true })
+            end, {})
+            vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader>s", builtin.git_status, {})
+            vim.keymap.set("n", "<leader>c",builtin.git_commits	, {})
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
